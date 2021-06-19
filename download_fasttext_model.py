d2ac50c93c648b64d58ec79192c51a2759dcc3a8
Kamil Kanclerz <kamil.kanclerz@pwr.edu.pl>
2021-06-20 01:55:15 +0200

    Change fasttext model download script and importing the model

--- download_fasttext_model.py
+++ download_fasttext_model.py
@@ -1,19 +1,24 @@
-import urllib.request
+import requests
 
-if __name__ == "__main__":
-    url = 'https://nextcloud.clarin-pl.eu/index.php/s/luubhnS0AvjmtQc/download?path=%2F&files=kgr10.plain.skipgram.dim300.neg10.bin'
-    response = urllib.request.urlopen(url)
 
-    local_filename = url.split('=')[-1]
+def download(url):
+    get_response = requests.get(url, stream=True)
+
+    file_name = url.split("=")[-1]
 
     totalbits = 0
 
-    if response.status_code == 200:
-        with open(local_filename, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    totalbits += 1024
-                    print("Downloaded", totalbits * 1025, "KB...")
-                    f.write(chunk)
+    with open(file_name, 'wb') as f:
+        for chunk in get_response.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                totalbits += 1024
+                print("Downloaded", totalbits * 1025, "KB...")
+                f.write(chunk)
 
-    # html = response.read()
+
+# download("https://example.com/example.jpg")
+
+if __name__ == "__main__":
+    url = 'https://nextcloud.clarin-pl.eu/index.php/s/luubhnS0AvjmtQc/download?path=%2F&files=kgr10.plain.skipgram.dim300.neg10.bin'
+
+    download(url)
--- generate_fasttext_embeddings.py
+++ generate_fasttext_embeddings.py
@@ -1,6 +1,11 @@
 import fasttext
 
+
 def load_fasttext():
     model = fasttext.load_model("kgr10.plain.skipgram.dim300.neg10.bin")
 
     return model
+
+
+if __name__ == '__main__':
+    ft_model = load_fasttext()
