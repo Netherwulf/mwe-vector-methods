855c638998d123cbd3704d59cfb9feec9a8d4142
Kamil Kanclerz <kanclerz3@tlen.pl>
2021-06-20 18:55:43 +0200

    Fix dataset generating script

--- generate_fasttext_embeddings.py
+++ generate_fasttext_embeddings.py
@@ -18,12 +18,15 @@ def read_mwe(file_path, incorrect_mwe_file=False):
 
         mwe_index = 0
 
-        for line in content:
+        for line_index, line in enumerate(content):
+            if line_index == 0:
+                continue
 
             if incorrect_mwe_file:
-                mwe = line.strip().split(',')[1]
+                if ',' in line:
+                    mwe = line.strip().split(',')[1].replace("\"", '')
             else:
-                mwe = line.strip().split('\t')[3]
+                mwe = line.strip().split('\t')[3].replace("\"", '')
 
             mwe_words = mwe.split(' ')
 
@@ -34,15 +37,17 @@ def read_mwe(file_path, incorrect_mwe_file=False):
             second_word = mwe_words[1]
 
             mwe_list[mwe_index] = (first_word, second_word)
+            mwe_index += 1
 
     # clean mwe_list from the dummy elements
     mwe_list = [mwe for mwe in mwe_list if mwe[0] != '*' * 200]
-
+    print(f'mwe_list len: {len(mwe_list)}')
     return mwe_list
 
 
 def generate_embeddings(ft_model, mwe_list, incorrect_mwe_list=False):
     embeddings_arr = np.empty((len(mwe_list), 901), dtype=np.float32)
+    print(f'len of mwe_list: {len(mwe_list)}')
 
     for mwe_index, mwe_words in enumerate(mwe_list):
         embeddings_arr[mwe_index][0: 300] = ft_model.get_word_vector(mwe_words[0])
@@ -77,7 +82,7 @@ def main(args):
         mwe_list = read_mwe(mwe_file_path, incorrect_mwe_file=are_mwes_incorrect)
         mwe_embeddings = generate_embeddings(ft_model, mwe_list, incorrect_mwe_list=are_mwes_incorrect)
         print(f'mwe_embeddings shape: {mwe_embeddings.shape}')
-        embeddings_arr = np.append(embeddings_arr, mwe_embeddings)
+        embeddings_arr = np.concatenate((embeddings_arr, mwe_embeddings), axis=0)
 
     print(f'embeddings_arr shape: {embeddings_arr.shape}')
     save_mwe_embeddings(output_file_name, embeddings_arr)
