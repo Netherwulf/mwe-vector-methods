4697e332f112bb92dc7aa6487d193eeed29ce270
Kamil Kanclerz <kanclerz3@tlen.pl>
2021-06-20 16:16:05 +0200

    Add generating embeddings and saving final dataset

--- download_fasttext_model.py
+++ download_fasttext_model.py
@@ -16,8 +16,6 @@ def download(url):
                 f.write(chunk)
 
 
-# download("https://example.com/example.jpg")
-
 if __name__ == "__main__":
     url = 'https://nextcloud.clarin-pl.eu/index.php/s/luubhnS0AvjmtQc/download?path=%2F&files=kgr10.plain.skipgram.dim300.neg10.bin'
 
--- generate_fasttext_embeddings.py
+++ generate_fasttext_embeddings.py
@@ -1,11 +1,87 @@
+import os
+
 import fasttext
+import numpy as np
 
 
-def load_fasttext():
-    model = fasttext.load_model("kgr10.plain.skipgram.dim300.neg10.bin")
+def load_fasttext(model_path):
+    model = fasttext.load_model(model_path)
 
     return model
 
 
+def read_mwe(file_path, incorrect_mwe_file=False):
+    with open(file_path, 'r', encoding='utf-8') as correct_mwe_file:
+        content = correct_mwe_file.readlines()
+
+        mwe_list = [('*' * 200, '*' * 200) for _ in range(len(content))]
+
+        mwe_index = 0
+
+        for line in content:
+
+            if incorrect_mwe_file:
+                mwe = line.strip().split(',')[1]
+            else:
+                mwe = line.strip().split('\t')[3]
+            mwe_words = mwe.split(' ')
+
+            if len(mwe_words) > 2:
+                continue
+
+            first_word = mwe_words[0]
+            second_word = mwe_words[1]
+
+            mwe_list[mwe_index] = (first_word, second_word)
+
+    # clean mwe_list from the dummy elements
+    mwe_list = [mwe for mwe in mwe_list if mwe[0] != '*' * 200]
+
+    return mwe_list
+
+
+def generate_embeddings(ft_model, mwe_list, incorrect_mwe_list=False):
+    embeddings_arr = np.empty((len(mwe_list), 4), dtype=np.float32)
+
+    for mwe_index, mwe_words in enumerate(mwe_list):
+        embeddings_arr[mwe_index][0] = ft_model.get_word_vector(mwe_words[0])
+        embeddings_arr[mwe_index][1] = ft_model.get_word_vector(mwe_words[1])
+        embeddings_arr[mwe_index][
+            2] = embeddings_arr[mwe_index][0] - embeddings_arr[mwe_index][1]
+        embeddings_arr[mwe_index][3] = 0.0 if incorrect_mwe_list else 1.0
+
+    return embeddings_arr
+
+
+def save_mwe_embeddings(output_file_name, mwe_embeddings) -> None:
+    # shuffle samples in the dataset
+    np.random.shuffle(mwe_embeddings)
+
+    # save dataset to .npy file
+    np.save(output_file_name, mwe_embeddings)
+
+
+def main(args):
+    ft_model_path = "kgr10.plain.skipgram.dim300.neg10.bin"
+    correct_mwe_file_path = 'correct_mwe.tsv'
+    incorrect_mwe_file_path = 'incorrect_MWE_kompozycyjne_polaczenia_plWN.csv'
+    output_file_name = 'mwe_dataset.npy'
+
+    ft_model = load_fasttext(ft_model_path)
+
+    embeddings_arr = np.empty((0, 4), dtype=np.float32)
+    print(f'embeddings_arr basic shape: {embeddings_arr.shape}')
+
+    for file_index, mwe_file_path in enumerate([correct_mwe_file_path, incorrect_mwe_file_path]):
+        are_mwes_incorrect = file_index == 1
+        mwe_list = read_mwe(mwe_file_path)
+        mwe_embeddings = generate_embeddings(ft_model, mwe_list, incorrect_mwe_list=are_mwes_incorrect)
+        print(f'mwe_embeddings shape: {mwe_embeddings.shape}')
+        embeddings_arr = np.append(embeddings_arr, mwe_embeddings)
+
+    print(f'embeddings_arr shape: {embeddings_arr.shape}')
+    save_mwe_embeddings(output_file_name, embeddings_arr)
+
+
 if __name__ == '__main__':
-    ft_model = load_fasttext()
+    main(os.argv[1:])
