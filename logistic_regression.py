3d064d5d8b061323835e0dbfb6633c2da8c2cfd3
Kamil <kanclerz3@tlen.pl>
2021-08-11 21:08:21 +0200

    Add saving lr model

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,3 +1,5 @@
+import pickle
+
 from sklearn.linear_model import LogisticRegression
 
 
@@ -12,6 +14,16 @@ def train_lr_model(model, X, y):
     return model
 
 
+def save_lr_model(model, filename):
+    pickle.dump(model, open(filename, 'wb'))
+
+
+def load_lr_model(filename):
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    return loaded_model
+
+
 def get_lr_model_predictions(model, X_test):
 
     return model.predict(X_test)
@@ -21,6 +33,8 @@ def get_lr_model_pred(X_train, y_train, X_test):
     lr_model = create_lr_model()
 
     lr_model = train_lr_model(lr_model, X_train, y_train)
+
+    save_lr_model(lr_model, 'lr_model.pkl')
 
     y_pred = get_lr_model_predictions(lr_model, X_test)
 
