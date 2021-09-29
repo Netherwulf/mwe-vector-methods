fde6a612cbd3c852dd9d85eccbc60f9d2b74fc46
Kamil <kanclerz3@tlen.pl>
2021-09-29 23:12:22 +0200

    Fix list to string conversion

--- generate_transformer_embeddings.py
+++ generate_transformer_embeddings.py
@@ -141,10 +141,10 @@ def read_tsv(filepath, tokenizer, model, layers, lemmatizer):
 
                 print(f'first_word_embedding: {type(first_word_embedding)}',
                       f'mwe_embedding: {type(mwe_embedding)}',
-                      f'first_word_mwe_emb_diff: {type(first_word_mwe_emb_diff)}',)
+                      f'first_word_mwe_emb_diff: {type(first_word_mwe_emb_diff)}', sep='\n')
 
                 write_line_to_file(complete_mwe_in_sent_output_file, '\t'.join(
-                    [first_word_embedding.numpy().tostring(), mwe_embedding.numpy().tostring(),
+                    [first_word_embedding.numpy().tostring(), ','.join(mwe_embedding),
                      ','.join(first_word_mwe_emb_diff), is_correct]))
 
 
