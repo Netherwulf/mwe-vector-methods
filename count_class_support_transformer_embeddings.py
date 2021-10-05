0085b44b86b9995bfb086ac4daa93e0e0c54ec84
Kamil Kanclerz <kamil.kanclerz@e-science.pl>
2021-10-05 15:54:16 +0200

    Change column name

--- count_class_support_transformer_embeddings.py
+++ count_class_support_transformer_embeddings.py
@@ -6,15 +6,15 @@ import pandas as pd
 def count_support(filepath):
     df = pd.read_csv(filepath, sep='\t', header=None, usecols=[3])
 
-    incorrect_count = df[0].tolist().count(0)
-    correct_count = df[0].tolist().count(1)
+    incorrect_count = df[3].tolist().count(0)
+    correct_count = df[3].tolist().count(1)
 
     print(f'incorrect samples: {incorrect_count}',
           f'correct samples: {correct_count}',
           f'incorrect / correct: {round(incorrect_count / correct_count, 2)}',
           f'correct / incorrect: {round(correct_count / incorrect_count, 2)}',
-          f'correct / all: {round(correct_count / len(df[0]), 2)}',
-          f'incorrect / all: {round(incorrect_count / len(df[0]), 2)}',
+          f'correct / all: {round(correct_count / len(df[3]), 2)}',
+          f'incorrect / all: {round(incorrect_count / len(df[3]), 2)}',
           sep='\n')
 
 
