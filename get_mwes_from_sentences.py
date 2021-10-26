ffd98fcd5fc3619a0747ed9043b58ffe92167961
Kamil <kanclerz3@tlen.pl>
2021-10-26 13:53:52 +0200

    Add extra data about MWE occurence

--- get_mwes_from_sentences.py
+++ get_mwes_from_sentences.py
@@ -25,7 +25,7 @@ def get_mwes(filepath, num_of_rows):
     with open(filepath, 'r', errors='replace') as in_file:
         content = in_file.readlines()
 
-        for line in content[1:]:
+        for line_idx, line in enumerate(content[1:]):
             line = line.strip()
 
             line_attributes = line.split('\t')
@@ -33,21 +33,28 @@ def get_mwes(filepath, num_of_rows):
             mwe = line_attributes[0]
             is_correct = line_attributes[2]
             complete_mwe_in_sent = line_attributes[3]
+            first_word_idx = line_attributes[4]
+            first_word_orth = line_attributes[5]
+            first_word_lemma = line_attributes[6]
+            sentence = line_attributes[12]
 
             # complete MWE appears in the sentence
             if complete_mwe_in_sent == '1':
                 write_line_to_file(complete_mwe_in_sent_output_file, '\t'.join(
-                    [mwe, is_correct]))
+                    [mwe, is_correct, first_word_idx, first_word_orth, first_word_lemma, sentence]))
 
             # only part of MWE appears in the sentence
             else:
                 write_line_to_file(incomplete_mwe_in_sent_output_file, '\t'.join(
-                    [mwe, is_correct]))
+                    [mwe, is_correct, first_word_idx, first_word_orth, first_word_lemma, sentence]))
+
+            if line_idx >= num_of_rows:
+                break
 
 
 def main(args):
     for filepath in args:
-        get_mwes(filepath, 4 * 10 ** 6)
+        get_mwes(filepath, 7 * 10 ** 6)
 
 
 if __name__ == '__main__':
