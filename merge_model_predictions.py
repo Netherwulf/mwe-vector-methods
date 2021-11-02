173633aef1693b61ce0966c4e9dec3651a848eb8
Kamil <kanclerz3@tlen.pl>
2021-11-02 23:07:32 +0100

    Add script for merging predictions

---
+++ merge_model_predictions.py
@@ -0,0 +1,39 @@
+import sys
+
+import pandas as pd
+
+
+def merge_prediction_results(filepath_list):
+    for filepath_idx, filepath in enumerate(filepath_list):
+        if filepath_idx == 0:
+            df = pd.read_csv(filepath, sep='\t')
+
+            df = df.rename(columns={df.columns[-1]: 'majority_cnn'})
+
+        else:
+            df_temp = pd.read_csv(filepath, sep='\t')
+
+            model_name = filepath.split('.')[0].split('_')[-3] + '_' + filepath.split('.')[0].split('_')[-1]
+
+            df[model_name] = df_temp['model_prediction']
+
+    final_df = df[(df['first_word_orth'] != 'po') & (df['first_word_lemma'] != 'po')]
+
+    return final_df
+
+
+def main(args):
+    filepath_list = ['results_transformer_embeddings_svm_smote_majority_voting_cnn.tsv',
+                     'results_transformer_embeddings_svm_smote_majority_voting_lr.tsv',
+                     'results_transformer_embeddings_svm_smote_majority_voting_rf.tsv',
+                     'results_transformer_embeddings_svm_smote_weighted_voting_cnn.tsv',
+                     'results_transformer_embeddings_svm_smote_weighted_voting_lr.tsv',
+                     'results_transformer_embeddings_svm_smote_weighted_voting_rf.tsv']
+
+    df = merge_prediction_results(filepath_list)
+
+    df.to_csv('merged_predictions.tsv', sep='\t', encoding='utf-8', index=False)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
