bc5fcb9ae0ff54a52d3794dd4d8f26c68b706c91
Kamil Kanclerz <kamil.kanclerz@pwr.edu.pl>
2021-11-05 00:13:07 +0100

    Add model visualization utility

---
+++ get_model_visualization.py
@@ -0,0 +1,34 @@
+import sys
+
+from cnn import create_cnn_model
+
+from tensorflow import keras.models
+from tensorflow.keras.utils import plot_model
+
+
+def load_model(filepath):
+    return keras.models.load_model(filepath)
+
+
+def visualize_model(filepath=None):
+    if filepath is None:
+        model = create_cnn_model(input_shape=(900, 1))
+
+    else:
+        model = load_model(filepath)
+
+    plot_model(model, to_file='model_visualization.png', show_shapes=True, show_dtype=False,
+        show_layer_names=True, rankdir='LR', expand_nested=False, dpi=300,
+        layer_range=None)
+
+def main(args):
+    if len(args) == 0:
+        visualize_model(None)
+
+    else:
+        for filepath in args:
+            visualize_model(filepath)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
