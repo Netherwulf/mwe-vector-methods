1b356075d78fa7e62f7ae49870d37fc2a537215c
Kamil <kanclerz3@tlen.pl>
2021-12-12 22:51:43 +0100

    Change generating fasttext embeddings

--- generate_fasttext_embeddings.py
+++ generate_fasttext_embeddings.py
@@ -2,6 +2,7 @@ import sys
 
 import fasttext
 import numpy as np
+import pandas as pd
 
 
 def load_fasttext(model_path):
@@ -10,43 +11,60 @@ def load_fasttext(model_path):
     return model
 
 
-def read_mwe(file_path, incorrect_mwe_file=False):
-    with open(file_path, 'r', encoding='utf-8') as correct_mwe_file:
-        content = correct_mwe_file.readlines()
+def read_mwe(file_path, ft_model, incorrect_mwe_file):
+    df = pd.read_csv(file_path, sep='\t')
+    df = df.drop_duplicates(subset=['full_mwe'])
 
-        mwe_list = [('*' * 200, '*' * 200) for _ in range(len(content))]
+    df['first_word_ft_emb'] = [','.join([str(elem) for elem in ft_model.get_word_vector(word)]) for word in
+                               df['first_word'].tolist()]
+    df['second_word_ft_emb'] = [','.join([str(elem) for elem in ft_model.get_word_vector(word)]) for word in
+                                df['second_word'].tolist()]
+    df['first_second_ft_emb_diff'] = [
+        ','.join([str(elem) for elem in (ft_model.get_word_vector(first_word) - ft_model.get_word_vector(second_word))])
+        for
+        first_word, second_word in
+        zip(df['first_word'].tolist(), df['second_word'].tolist())]
 
-        mwe_index = 0
+    df['is_correct'] = '0' if incorrect_mwe_file else '1'
 
-        for line_index, line in enumerate(content):
-            if line_index == 0:
-                continue
+    return df
 
-            # get MWEs from KGR10 (Słowosieć) MWE lists
-            # if incorrect_mwe_file:
-            #     if ',' in line:
-            #         mwe = line.strip().split(',')[1].replace("\"", '')
-            # else:
-            #     mwe = line.strip().split('\t')[3].replace("\"", '')
-
-            # get MWEs from PARSEME MWE lists
-            mwe = line.strip().split('\t')[7]
-
-            mwe_words = mwe.split(' ')
-
-            if len(mwe_words) != 2:
-                continue
-
-            first_word = mwe_words[0]
-            second_word = mwe_words[1]
-
-            mwe_list[mwe_index] = (first_word, second_word)
-            mwe_index += 1
-
-    # clean mwe_list from the dummy elements
-    mwe_list = [mwe for mwe in mwe_list if mwe[0] != '*' * 200]
-
-    return mwe_list
+    # with open(file_path, 'r', encoding='utf-8') as correct_mwe_file:
+    #     content = correct_mwe_file.readlines()
+    #
+    #     mwe_list = [('*' * 200, '*' * 200) for _ in range(len(content))]
+    #
+    #     mwe_index = 0
+    #
+    #     for line_index, line in enumerate(content):
+    #         if line_index == 0:
+    #             continue
+    #
+    #         # get MWEs from KGR10 (Słowosieć) MWE lists
+    #         # if incorrect_mwe_file:
+    #         #     if ',' in line:
+    #         #         mwe = line.strip().split(',')[1].replace("\"", '')
+    #         # else:
+    #         #     mwe = line.strip().split('\t')[3].replace("\"", '')
+    #
+    #         # get MWEs from PARSEME MWE lists
+    #         mwe = line.strip().split('\t')[7]
+    #
+    #         mwe_words = mwe.split(' ')
+    #
+    #         if len(mwe_words) != 2:
+    #             continue
+    #
+    #         first_word = mwe_words[0]
+    #         second_word = mwe_words[1]
+    #
+    #         mwe_list[mwe_index] = (first_word, second_word)
+    #         mwe_index += 1
+    #
+    # # clean mwe_list from the dummy elements
+    # mwe_list = [mwe for mwe in mwe_list if mwe[0] != '*' * 200]
+    #
+    # return mwe_list
 
 
 def get_count_dict(file_path, incorrect_mwe_file=False, type_based=False):
@@ -122,7 +140,8 @@ def read_mwe_balanced(file_path, count_dict, final_sample_size=4064, incorrect_m
                     domain = line.strip().split('\t')[2]
 
             if domain in mwe_domain_dict.keys() \
-                    and mwe_domain_dict[domain] >= int((count_dict[domain] / imbalanced_sample_size) * final_sample_size):
+                    and mwe_domain_dict[domain] >= int(
+                (count_dict[domain] / imbalanced_sample_size) * final_sample_size):
                 continue
 
             mwe_words = mwe.split(' ')
@@ -185,24 +204,31 @@ def main(args):
 
     ft_model = load_fasttext(ft_model_path)
 
-    embeddings_arr = np.empty((0, 901), dtype=np.float32)
+    # embeddings_arr = np.empty((0, 901), dtype=np.float32)
 
-    for file_index, mwe_file_path in enumerate([correct_mwe_file_path, incorrect_mwe_file_path]):
-        are_mwes_incorrect = file_index == 1
+    correct_mwe_df = read_mwe(correct_mwe_file_path, ft_model, False)
+    incorrect_mwe_df = read_mwe(incorrect_mwe_file_path, ft_model, True)
 
-        # imbalanced mwe dataset generation
-        mwe_list = read_mwe(mwe_file_path, incorrect_mwe_file=are_mwes_incorrect)
+    mwe_df = correct_mwe_df.append(incorrect_mwe_df)
+
+    mwe_df.to_csv(output_file_name, index=False)
+
+    # for file_index, mwe_file_path in enumerate([correct_mwe_file_path, incorrect_mwe_file_path]):
+    #     are_mwes_incorrect = file_index == 1
+    #
+    #     # imbalanced mwe dataset generation
+    #     mwe_list = read_mwe(mwe_file_path, incorrect_mwe_file=are_mwes_incorrect)
 
         # domain balanced or type balanced mwe dataset generation
         # count_dict = get_count_dict(mwe_file_path, incorrect_mwe_file=are_mwes_incorrect, type_based=True)
         # mwe_list = read_mwe_balanced(mwe_file_path, count_dict, final_sample_size=4064,
         #                              incorrect_mwe_file=are_mwes_incorrect, type_based=True)
 
-        mwe_embeddings = generate_embeddings(ft_model, mwe_list, incorrect_mwe_list=are_mwes_incorrect)
+        # mwe_embeddings = generate_embeddings(ft_model, mwe_list, incorrect_mwe_list=are_mwes_incorrect)
+        #
+        # embeddings_arr = np.concatenate((embeddings_arr, mwe_embeddings), axis=0)
 
-        embeddings_arr = np.concatenate((embeddings_arr, mwe_embeddings), axis=0)
-
-    save_mwe_embeddings(output_file_name, embeddings_arr)
+    # save_mwe_embeddings(output_file_name, embeddings_arr)
 
 
 if __name__ == '__main__':
