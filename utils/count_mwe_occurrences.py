ddf2c4b8fced6abbed82813f65bcb9ee7c571d00
Kamil Kanclerz <kamil.kanclerz@e-science.pl>
2022-03-11 00:57:36 +0100

    Add notebook for filtering MWE lists and scripts for replacing sample labels

--- utils/count_mwe_occurrences.py
+++ utils/count_mwe_occurrences.py
@@ -1,22 +1,50 @@
-import csv
+import argparse
 import string
-import sys
 
 import numpy as np
 
-from datetime import datetime
+import datetime
 from typing import List
 
 import morfeusz2
 
 
+def log_message(message):
+    print(f'{datetime.datetime.now().strftime("%H:%M:%S")} : {message}')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_path',
+                        help='path to the file containing sentences',
+                        type=str)
+    parser.add_argument('--data_mwe_col_idx',
+                        help='index of column containing MWE in sentence_file',
+                        type=int)
+    parser.add_argument('--mwe_list_path', help='path to MWE list', type=str)
+    parser.add_argument('--mwe_col_idx',
+                        help='index of column containing MWE in MWE list',
+                        type=int)
+
+    args = parser.parse_args()
+
+    return args
+
+
 # read MWEs from Słowosieć tsv or csv file
-def read_mwe(filepath, separator, column_index) -> []:
-    with open(filepath, 'r', encoding="utf-8") as f:
-        content = list(csv.reader(f, delimiter=separator, quotechar='"'))
-        mwe_list = [sublist[column_index] for sublist in content[1:] if len(sublist) != 0]
-        mwe_list = [mwe for mwe in mwe_list if not any(character.isupper() for character in mwe)]
-        return mwe_list
+# def read_mwe(filepath, separator, column_index) -> List[str]:
+#     with open(filepath, 'r', encoding="utf-8") as f:
+#         content = list(csv.reader(f, delimiter=separator, quotechar='"'))
+#         mwe_list = [
+#             sublist[column_index] for sublist in content[1:]
+#             if len(sublist) != 0
+#         ]
+#         mwe_list = [
+#             mwe for mwe in mwe_list
+#             if not any(character.isupper() for character in mwe)
+#         ]
+#         return mwe_list
 
 
 # init Morfeusz2 lemmatizer
@@ -24,14 +52,24 @@ def init_lemmatizer():
     return morfeusz2.Morfeusz()  # initialize Morfeusz object
 
 
+# lemmatize single MWE
+def lemmatize_single_mwe(mwe, lemmatizer) -> string:
+
+    mwe_words = [token for token in mwe.split(' ')]
+    lemmatized_mwe = ' '.join([
+        str(lemmatizer.analyse(word)[0][2][1])
+        if word not in string.punctuation else word for word in mwe_words
+    ])
+
+    return lemmatized_mwe
+
+
 # lemmatize MWEs
-def lemmatize_mwe(mwe_list, lemmatizer) -> List[str]:
+def lemmatize_mwe_list(mwe_list, lemmatizer) -> List[str]:
     lemmatized_mwe_list = ['*' * 200 for _ in range(len(mwe_list))]
 
     for i, mwe in enumerate(mwe_list):
-        mwe_words = [token for token in mwe.split(' ')]
-        lemmatized_mwe_list[i] = ' '.join(
-            [str(lemmatizer.analyse(word)[0][2][1]) if word not in string.punctuation else word for word in mwe_words])
+        lemmatized_mwe_list[i] = lemmatize_single_mwe(mwe, lemmatizer)
 
     return lemmatized_mwe_list
 
@@ -43,72 +81,136 @@ def clean_mwe_list(mwe_list, lemmatized_mwe_list):
             lemmatized_mwe_list[i] = mwe.replace('\xa0', ' ')
 
     mwe_list = [mwe for mwe in mwe_list if len(mwe.split(' ')) == 2]
-    lemmatized_mwe_list = [mwe for mwe in lemmatized_mwe_list if len(mwe.split(' ')) == 2]
+    lemmatized_mwe_list = [
+        mwe for mwe in lemmatized_mwe_list if len(mwe.split(' ')) == 2
+    ]
 
     return mwe_list, lemmatized_mwe_list
 
 
-def load_mwes(mwe_file):
-    lemmatizer = init_lemmatizer()
+# def load_mwes(mwe_file):
+#     lemmatizer = init_lemmatizer()
 
-    mwe_list = read_mwe(mwe_file, '\t', 2)
-    lemmatized_mwes = lemmatize_mwe(mwe_list, lemmatizer)
+#     mwe_list = read_mwe(mwe_file, '\t', 2)
+#     lemmatized_mwes = lemmatize_mwe(mwe_list, lemmatizer)
 
-    mwe_list, lemmatized_mwes = clean_mwe_list(mwe_list, lemmatized_mwes)
+#     mwe_list, lemmatized_mwes = clean_mwe_list(mwe_list, lemmatized_mwes)
 
-    return mwe_list, lemmatized_mwes
+#     return mwe_list, lemmatized_mwes
 
 
-def count_mwe_occurrences(mwe_list, lemmatized_mwe_list, filepath):
-    mwe_dict = {lemmatized_mwe: 0 for lemmatized_mwe in lemmatized_mwe_list}
+def read_mwe(mwe_filepath,
+             column_index,
+             lemmatizer,
+             separator='\t') -> List[str]:
+    mwe_list = []
 
-    with open(filepath, 'r', encoding='utf-8') as in_file:
-        line_idx = 0
+    line_idx = 0
 
-        # skip first line
-        line = in_file.readline()
+    with open(mwe_filepath, 'r', encoding="utf-8") as f:
+        for line in f:
 
-        while line:
-            line = in_file.readline()
-
-            if len(line.split('\t')) < 2:
+            # skip first line
+            if line_idx == 0:
+                line_idx += 1
                 continue
 
-            lemmatized_mwe = line.split('\t')[1]
+            line = line.strip()
+            line_elems = line.split(separator)
 
-            mwe_dict[lemmatized_mwe] += 1
+            # check if line is empty
+            if len(line_elems) < 2:
+                line_idx += 1
+                continue
 
-            if line_idx % 10000000 == 0 and line_idx > 0:
-                print(f'{datetime.now().time()} - Processed {line_idx} files')
+            mwe_list.append(line_elems[column_index])
 
             line_idx += 1
 
-        mwe_occurrences = [occurrence_count for occurrence_count in mwe_dict.values()]
+        lemmatized_mwe_list = lemmatize_mwe_list(mwe_list, lemmatizer)
+
+    return lemmatized_mwe_list
+
+
+def count_mwe_occurrences(mwe_list,
+                          data_filepath,
+                          data_mwe_col_idx,
+                          lemmatizer,
+                          separator='\t'):
+    mwe_dict = {lemmatized_mwe: 0 for lemmatized_mwe in mwe_list}
+
+    occurrence_count = 0
+
+    with open(data_filepath, 'r', encoding='utf-8') as in_file:
+
+        for line in in_file:
+
+            # skip first line
+            if occurrence_count == 0:
+                occurrence_count += 1
+                continue
+
+            line = line.strip()
+
+            line_elems = line.split(separator)
+
+            if len(line_elems) < 2:
+                continue
+
+            mwe = line_elems[data_mwe_col_idx]
+
+            lemmatized_mwe = lemmatize_single_mwe(mwe, lemmatizer)
+
+            if lemmatized_mwe not in mwe_dict.keys():
+                continue
+
+            else:
+                mwe_dict[lemmatized_mwe] += 1
+
+            if occurrence_count % 10000 == 0 and occurrence_count > 0:
+                log_message(f'Processed {occurrence_count} files')
+
+            occurrence_count += 1
+
+        mwe_occurrences = [
+            occurrence_count for occurrence_count in mwe_dict.values()
+        ]
+
         empty_mwe_dict = [mwe for mwe in mwe_dict.keys() if mwe_dict[mwe] == 0]
 
-        print(f'{datetime.now().time()} - Results for file: {filepath}',
+        print(f'Results for file: {data_filepath}',
               f'Max occurrences: {np.max(mwe_occurrences)}',
               f'Median occurrences: {np.median(mwe_occurrences)}',
               f'Mean occurrences: {np.mean(mwe_occurrences)}',
               f'Min occurrences: {np.min(mwe_occurrences)}',
               f'Zero occurrences count: {len(empty_mwe_dict)}',
-              f'Total MWE count: {len(lemmatized_mwe_list)}',
-              f'Total MWE occurrences count: {line_idx}',
+              f'Total MWE count: {len(mwe_list)}',
+              f'Total MWE occurrences count: {sum(mwe_occurrences)}',
               sep='\n')
 
 
-def main(args):
-    mwes_filepath = 'scaled_vector_association_measure_correct_mwe_best_f1.tsv'
+def main():
+    args = parse_args()
 
-    print(f'{datetime.now().time()} - Reading MWE files and lemmatizing...')
+    data_path = args.data_path
+    data_mwe_col_idx = args.data_mwe_col_idx
+    mwe_list_path = args.mwe_list_path
+    mwe_col_idx = args.mwe_col_idx
 
-    mwe_list, lemmatized_mwes = load_mwes(mwes_filepath)
+    lemmatizer = init_lemmatizer()
 
-    print(f'{datetime.now().time()} - Finished reading MWE files and lemmatizing...')
+    # mwes_filepath = 'scaled_vector_association_measure_correct_mwe_best_f1.tsv'
 
-    for filepath in args:
-        count_mwe_occurrences(mwe_list, lemmatized_mwes, filepath)
+    log_message('Reading MWE files...')
+
+    # mwe_list, lemmatized_mwes = load_mwes(mwes_filepath)
+
+    mwe_list = read_mwe(mwe_list_path, mwe_col_idx, lemmatizer)
+
+    log_message('Finished reading MWE files...')
+
+    count_mwe_occurrences(mwe_list, data_path, data_mwe_col_idx, lemmatizer)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
---
+++ utils/replace_sample_labels.py
@@ -0,0 +1,84 @@
+import argparse
+
+from datetime import datetime
+
+from utils import read_mwe, init_lemmatizer, lemmatize_single_mwe
+
+
+def log_message(message):
+    print(f'{datetime.datetime.now().strftime("%H:%M:%S")} : {message}')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_path',
+                        help='path to the file containing sentences',
+                        type=str)
+    parser.add_argument('--data_mwe_idx',
+                        help='index of column containing MWE in data file',
+                        type=str)
+    parser.add_argument('--label_idx',
+                        help='index of column containing labels',
+                        type=int)
+    parser.add_argument('--mwe_list_path', help='path to MWE list', type=str)
+    parser.add_argument('--output_path',
+                        help='path to the output file',
+                        type=str)
+    parser.add_argument('--mwe_col_idx',
+                        help='index of column containing MWE in MWE list',
+                        type=int)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def replace_sample_labels(data_path,
+                          data_mwe_idx,
+                          label_idx,
+                          mwe_list_path,
+                          mwe_col_idx,
+                          output_path,
+                          separator='\t'):
+    lemmatizer = init_lemmatizer()
+
+    mwe_list = read_mwe(mwe_list_path, mwe_col_idx, lemmatizer)
+
+    with open(data_path, 'r') as in_file, open(output_path, 'w') as out_file:
+        line_idx = 0
+
+        for line in in_file:
+            line_elems = line.strip().split(separator)
+
+            mwe = line_elems[data_mwe_idx]
+
+            lemmatized_mwe = lemmatize_single_mwe(mwe, lemmatizer)
+
+            if lemmatized_mwe in mwe_list:
+                line_elems[label_idx] = 0
+
+            out_file.write('\t'.join(line_elems) + '\n')
+
+            line_idx += 1
+
+            if line_idx != 0 and line_idx % 10000 == 0:
+                log_message(f'Processed {line_idx} lines')
+
+
+def main():
+    args = parse_args()
+
+    data_path = args.data_path
+    data_mwe_idx = args.data_mwe_idx
+    label_idx = args.label_idx
+    mwe_list_path = args.mwe_list_path
+    output_path = args.output_path
+    mwe_col_idx = args.mwe_col_idx
+
+    replace_sample_labels(data_path, data_mwe_idx, label_idx, mwe_list_path,
+                          mwe_col_idx, output_path)
+
+
+if __name__ == '__main__':
+    main()
